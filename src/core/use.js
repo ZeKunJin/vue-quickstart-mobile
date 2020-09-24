ffad39c487d4eaa9f371787ea76794f48fd531b8
ZeKunJin <2804767996@qq.com>
2020-09-24 11:06:01 +0800

    import composition api

--- src/core/use.js
+++ src/core/use.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueStorage from 'vue-ls'
+import VueCompositionAPI from '@vue/composition-api'
 import config from '@/config/defaultSettings'
 
+Vue.use(VueCompositionAPI)
 Vue.use(VueStorage, config.storageOptions)
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,59 @@
 <template>
-  <div class="home-page-layout">home page</div>
+  <div class="home-page-layout">
+    <h1>home page</h1>
+    count: {{ state.count }}<br />
+    computed count: {{ countPlusOne }}<br />
+    <van-button size="small" type="primary" @click="actions.add">add</van-button>
+  </div>
 </template>
+
+<script>
+import Vue from 'vue'
+import { reactive, onMounted, computed, watchEffect, watch } from '@vue/composition-api'
+import { Button } from 'vant'
+Vue.use(Button)
+
+export default {
+  setup(props, context) {
+    const state = reactive({
+      count: 0
+    })
+
+    const countPlusOne = computed({
+      get: () => state.count + 1,
+      set() {}
+    })
+
+    const stop = watchEffect(() => console.log(`computed count value: ${state.count}`))
+
+    watch(
+      () => state.count,
+      (count, prevCount) => {
+        console.log(`watch prevent count value: ${prevCount}`)
+      }
+    )
+
+    onMounted(() => {
+      console.log('on conponent mounted')
+    })
+
+    const actions = {
+      add() {
+        state.count++
+
+        // 停止 watchEffect 监听
+        state.count > 5 && stop()
+      }
+    }
+
+    return {
+      state,
+      actions,
+      countPlusOne
+    }
+  }
+}
+</script>
 
 <style lang="less" scoped>
 .home-page-layout {
